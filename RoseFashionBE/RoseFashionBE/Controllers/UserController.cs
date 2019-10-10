a9964ff449fd68e089e92b3494c925b6f4ab09a1
Yamada-Kenji <34558694+Yamada-Kenji@users.noreply.github.com>
2019-10-10 09:22:42 +0700

    Commit 10/10/2019
    Tạo CSDL bằng SQL Server
    Tạo UserController và UserModel trong API

---
+++ RoseFashionBE/RoseFashionBE/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using RoseFashionBE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+
+namespace RoseFashionBE.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Register(UserModel user)
+        {
+            
+            return Ok();
+        }
+
+        string Md5Encryption(string password) //from docs.microsoft.com
+        {
+            MD5 md5hash = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] hasharray= md5hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder stringbuilder = new StringBuilder();
+
+            for(int i = 0; i < hasharray.Length; i++)
+            {
+                stringbuilder.Append(hasharray[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return stringbuilder.ToString();
+        }
+    }
+}
---
+++ RoseFashionBE/RoseFashionBE/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoseFashionBE.Models
+{
+    public class UserModel
+    {
+        public string Username;
+        public string Password;
+        public string FullName;
+        public string Gender;
+        public DateTime DOB;
+        public string Email;
+        public string Address;
+        public string Phone;
+        public string Role;
+    }
+}
